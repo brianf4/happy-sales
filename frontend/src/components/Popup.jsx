7570c2a69cba4d5eaf158aa53e84d01595f3e11f
Brian Fuentes <Brian.Fuentes04@gmail.com>
2023-03-06 11:21:50 -0800

    fixing popup scan

--- frontend/src/components/Popup.jsx
+++ frontend/src/components/Popup.jsx
@@ -5,11 +5,23 @@ function Popup() {
 
   const [camera, setCamera] = useState(false);
   const [result, setResult] = useState(null);
+  const [id, setId] = useState({
+    id: ""
+  })
+
+  function handlechange(event) {
+    setId(prevId => {
+      return {
+        ...prevId,
+        [event.target.name]: result
+      }
+    })
+  }
 
   function onDetected(result) {
     setResult(result);
-    console.log('result', result)
     setCamera(false)
+    console.log('result', result)
     Quagga.stop()
   };
 
@@ -18,14 +30,21 @@ function Popup() {
       {/* Put this part before </body> tag */}
       <input type="checkbox" id="my-modal-4" className="modal-toggle" />
       <label htmlFor="my-modal-4" className="modal cursor-pointer items-center">
-        <label className="modal-box relative rounded-2xl" htmlFor="">
-          <div className="flex">
+        <label className="modal-box relative rounded-2xl flex flex-col gap-y-2" htmlFor="">
+          <div className="flex gap-x-2">
             <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
             <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
           </div>
-          <div className="flex">
+          <div className="flex gap-x-2">
             <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+            <input 
+            value={id.id} 
+            onChange={handlechange}
+            name="id"
+            type="text" 
+            placeholder="Scan for ID..." 
+            className="input input-bordered w-full max-w-xs" 
+             required/>
           </div>
           <p>{result ? result : "Scanning..."}</p>
           <button onClick={() => setCamera((prevCamera) => !prevCamera)}>
