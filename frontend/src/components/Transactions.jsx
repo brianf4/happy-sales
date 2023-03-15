c24c810ad915a648cb236cc06e5339ee0326bb7c
Brian Fuentes <Brian.Fuentes04@gmail.com>
2023-03-15 00:27:26 -0700

    trying to add nums to columns

--- frontend/src/components/Transactions.jsx
+++ frontend/src/components/Transactions.jsx
@@ -1,13 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 
 function Transactions(props) {
-
   function handleClick() {
     props.handleId(props.inventory._id)
     console.log(props.inventory._id)
   }
-
+  
 
   return (
       <tbody>
