5786d9103bb43fc06ad2551cd1df2dc1c60cabf4
Brian Fuentes <Brian.Fuentes04@gmail.com>
2023-03-16 18:50:27 -0700

    working ui

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function Sidebar() {
 
   return (
-    <div className="drawer-side">
+    <div className="drawer-side ">
       <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
       <ul className="menu p-4 w-80 bg-base-100 text-base-content">
         {/* <!-- Sidebar content here --> */}
