3f592842e950a98e52b21819eac3d97d9474ec89
Brian Fuentes <Brian.Fuentes04@gmail.com>
2023-04-30 15:49:19 -0700

    storytelling

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -35,7 +35,7 @@ function Index () {
             <span className="block">A Beautiful Modern</span>
             <span className="block">Sales Mangement Site</span>
           </h1>
-          <p className="py-6 max-w-lg">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <p className="py-6 max-w-lg lg:text-lg">Happy Sales is the premium solution for managing your product sales with ease. With Happy Sales, you'll have access to comprehensive reports and analytics that will give you valuable insights into your business performance. </p>
           <Link className="px-6 btn btn-primary" to="signup">Sign Up</Link>
         </div>
       </div>
@@ -46,11 +46,48 @@ function Index () {
     <div id="howItWorks" className="hero">
       <div className="hero-content flex flex-col w-full">
         <h2 className="py-10 text-4xl font-semibold">How it works:</h2>
-        <div className="flex flex-col gap-y-10 lg:flex-row lg:justify-between lg:w-full">
-          <div className="max-w-md">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:justify-center lg:gap-x-8 lg:w-full">
+          <div className="max-w-md flex flex-col gap-y-6">
             <span className="block text-primary font-bold">GET STARTED</span>
-            <h1 className="text-5xl font-bold">Learn how to get started with these steps.</h1>
-            <p className="py-6 text-lg">See how you can get the most out of managing your inventory and the products you sell.</p>
+            <h1 className="text-5xl font-bold">Easy to <span className="text-primary">Get Started</span></h1>
+            <section className="flex flex-col gap-y-8">
+              <section className="flex gap-x-8">
+                <div>
+                    <span className="text-2xl text-secondary">01</span>
+                </div>
+                <div>
+                  <span className="block text-lg font-bold">Generate</span>
+                  <span className="block">Generate barcodes for your porduct</span>
+                </div>
+              </section>
+              <section className="flex gap-x-8">
+                <div>
+                    <span className="text-2xl text-secondary">02</span>
+                </div>
+                <div>
+                  <span className="block text-lg font-bold">Add</span>
+                  <span className="block">Add the product your inventory</span>
+                </div>
+              </section>
+              <section className="flex gap-x-8">
+                <div>
+                    <span className="text-2xl text-secondary">03</span>
+                </div>
+                <div>
+                  <span className="block text-lg font-bold">Sell</span>
+                  <span className="block">Sell your ready to be sold product</span>
+                </div>
+              </section>
+              <section className="flex gap-x-8">
+                <div>
+                    <span className="text-2xl text-secondary">04</span>
+                </div>
+                <div>
+                  <span className="block text-lg font-bold">Review</span>
+                  <span className="block">Review your products overall performance</span>
+                </div>
+              </section>
+            </section>
           </div>
           <div>
             <img className="max-w-md rounded-lg shadow-2xl" src={undrawFollow} alt="" />
@@ -64,9 +101,9 @@ function Index () {
     <div className="hero">
       <div className="py-20 hero-content flex flex-col lg:flex-row-reverse lg:w-full lg:justify-between">
         <div className="max-w-lg">
-          <span className="block text-primary font-bold">GET STARTED</span>
+          <span className="block text-primary font-bold">CREATE</span>
           <h1 className="text-5xl font-bold">Generate your barcodes</h1>
-          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <p className="py-6 lg:text-lg">Effortlessly generate unique barcodes for your products and easily add them to your inventory for organized tracking and management.</p>
         </div>
         <div>
           <img className="max-w-md rounded-lg shadow-2xl" src={barcodeGif} alt="" />
@@ -79,9 +116,9 @@ function Index () {
     <div className="hero">
       <div className="py-20 hero-content flex flex-col lg:flex-row lg:w-full lg:justify-between">
         <div className="max-w-lg">
-          <span className="block text-primary font-bold">GET STARTED</span>
+          <span className="block text-primary font-bold">SCAN</span>
           <h1 className="text-5xl font-bold">Add your product to the inventory</h1>
-          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <p className="py-6 lg:text-lg">Managing your inventory is now easier than ever. Simply scan your barcode id into the form and add a name, cost, and the amount of you have on hand.</p>
         </div>
         <div>
           <img className="max-w-md rounded-lg shadow-2xl" src={addProductGif} alt="" />
@@ -90,13 +127,12 @@ function Index () {
     </div>
 
 
-
     <div className="py-20 hero">
       <div className="hero-content flex flex-col lg:flex-row-reverse lg:w-full lg:justify-between">
         <div className="max-w-lg">
-          <span className="block text-primary font-bold">GET STARTED</span>
+          <span className="block text-primary font-bold">SELL</span>
           <h1 className="text-5xl font-bold">Scan the items ready to be sold</h1>
-          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <p className="py-6 lg:text-lg">With Happy Sales, selling your products is a breeze. Scan items that are ready to be sold to the sales queue to see the transactions. </p>
         </div>
         <div>
           <img className="max-w-md rounded-lg shadow-2xl" src={salesGif} alt="" />
@@ -109,9 +145,9 @@ function Index () {
     <div className="py-20 hero">
       <div className="hero-content flex flex-col lg:flex-row lg:w-full lg:justify-between">
         <div className="max-w-lg">
-          <span className="block text-primary font-bold">GET STARTED</span>
+          <span className="block text-primary font-bold">REVIEW</span>
           <h1 className="text-5xl font-bold">Review and manage</h1>
-          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <p className="py-6 lg:text-lg">With Happy Sales, you'll have access to comprehensive reports and analytics that will give you valuable insights into your business performance. Whether you're a small business owner or a seasoned professional, Happy Sales has everything you need to optimize your sales and drive growth.</p>
         </div>
         <div>
           <img className="max-w-md rounded-lg shadow-2xl" src={statsGif} alt="" />
